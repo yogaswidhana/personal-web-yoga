74b23829d43fb71ab723ccba82c8fdc5a89f7c7e
Yoga Swara Widhana <78964295+yogaswidhana@users.noreply.github.com>
2023-08-29 01:58:42 +0800

    Update PortfolioSectionComponent.tsx

--- src/components/PortfolioSectionComponent.tsx
+++ src/components/PortfolioSectionComponent.tsx
@@ -39,7 +39,7 @@ function PortfolioSectionComponent(): React.JSX.Element {
           rowGap={{ base: 3, lg: 5 }}
         >
           <SectionSubtitle>Project I&apos;ve Created💼</SectionSubtitle>
-          <SectionDescription>I&apos;ve created a few project while I was learing about frontend website development. And all of the project that I&apos;ll explain below</SectionDescription>
+          <SectionDescription>I&apos;ve created a few project while I was learning about frontend website development. And all of the project that I&apos;ve been finished will be explained below</SectionDescription>
         </Flex>
 
         {/* Portfolio Section Content Container */}
