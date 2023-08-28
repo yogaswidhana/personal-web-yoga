d2d6c82fa66ea99029cad0b5db8aef8b94f4ae15
Yoga Swara Widhana <78964295+yogaswidhana@users.noreply.github.com>
2023-08-29 01:49:05 +0800

    Update education.ts

--- src/constants/education.ts
+++ src/constants/education.ts
@@ -2,13 +2,13 @@ import type { Education } from "@/types";
 
 export const educationList: Education[] = [
   {
-    school: "Indonesian Muslim University",
-    competence: "Informatics Engineering",
-    years: "2017 - 2022",
-  },
-   {
     school: "Senior High School 1 Bajeng",
     competence: "Science",
     years: "2013 - 2016",
+  },
+  {
+    school: "Indonesian Muslim University",
+    competence: "Informatics Engineering",
+    years: "2017 - 2022",
   },
 ];
