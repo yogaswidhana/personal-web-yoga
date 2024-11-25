22c9355d17532d54cdce5cc0552d2c7f495f286b
Yoga Swara Widhana <78964295+yogaswidhana@users.noreply.github.com>
2024-11-25 16:19:16 +0800

    Update portfolio.ts
    update terbaru

--- src/constants/portfolio.ts
+++ src/constants/portfolio.ts
@@ -6,21 +6,12 @@ export const portfolioList: Portfolio[] = [
     imageThumbnail: "/assets/images/Project8.webp",
     description:
       "This is the second personal branding project that I've created using Next JS, Tailwind CSS, and Framer Motion. This project is a website which showing about my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "personal-web-yoga.vercel.app",
+    livePreviewURL: "https://personal-web-yoga.vercel.app",
     repositoryURL: "https://github.com/yogaswidhana/personal-web-yoga.git",
     techStack: [
-      {
-        tech: "HTML",
-        logo: "/assets/svg/next js.svg",
-      },
-      {
-        tech: "CSS",
-        logo: "/assets/svg/tailwind.svg",
-      },
-      {
-        tech: "JavaScript",
-        logo: "/assets/svg/framer motion.svg",
-      },
+      { tech: "HTML", logo: "/assets/svg/next js.svg" },
+      { tech: "CSS", logo: "/assets/svg/tailwind.svg" },
+      { tech: "JavaScript", logo: "/assets/svg/framer motion.svg" },
     ],
   },
   {
@@ -31,266 +22,102 @@ export const portfolioList: Portfolio[] = [
     livePreviewURL: "",
     repositoryURL: "",
     techStack: [
-      {
-        tech: "HTML",
-        logo: "/assets/svg/html.svg",
-      },
-      {
-        tech: "PHP",
-        logo: "/assets/svg/php.svg",
-      },
-      {
-        tech: "CSS",
-        logo: "/assets/svg/css.svg",
-      },
-      {
-        tech: "JavaScript",
-        logo: "/assets/svg/javascript.svg",
-      },
-      {
-        tech: "CodeIgniter",
-        logo: "/assets/svg/codeigniter.svg",
-      },
+      { tech: "HTML", logo: "/assets/svg/html.svg" },
+      { tech: "PHP", logo: "/assets/svg/php.svg" },
+      { tech: "CSS", logo: "/assets/svg/css.svg" },
+      { tech: "JavaScript", logo: "/assets/svg/javascript.svg" },
+      { tech: "CodeIgniter", logo: "/assets/svg/codeigniter.svg" },
     ],
   },
   {
     title: "Panama Coffee Landing Pages",
     imageThumbnail: "/assets/images/Project2.webp",
     description:
-      "Petabyte Landing Page is a website that I created using HTML, CSS, and Bootstrap. This website displays information about a software house that operates in the field of education, such as application, website, and educational game creation services. This website also has portfolio, testimonial, and consultation features that make it easy for visitors to know the quality and excellence of this software house.",
+      "Panama Coffee Landing Page is a website that I created using HTML, CSS, and Bootstrap. This website displays information about a coffee shop and includes features like portfolio, testimonials, and consultation forms to enhance visitor engagement.",
     livePreviewURL: "",
     repositoryURL: "https://github.com/yogaswidhana/coffeeshop.git",
     techStack: [
-      {
-        tech: "HTML",
-        logo: "/assets/svg/html.svg",
-      },
-      {
-        tech: "CSS",
-        logo: "/assets/svg/css.svg",
-      },
-      {
-        tech: "Bootstrap",
-        logo: "/assets/svg/bootstrap.svg",
-      },
-      {
-        tech: "JavaScript",
-        logo: "/assets/svg/javascript.svg",
-      },
+      { tech: "HTML", logo: "/assets/svg/html.svg" },
+      { tech: "CSS", logo: "/assets/svg/css.svg" },
+      { tech: "Bootstrap", logo: "/assets/svg/bootstrap.svg" },
+      { tech: "JavaScript", logo: "/assets/svg/javascript.svg" },
     ],
   },
   {
     title: "Rizqi Computindo Landing Pages",
     imageThumbnail: "/assets/images/Project3.webp",
     description:
-      "Rizqi Computindo is a website that I created using HTML, PHP, CSS, JAVASCRIPT and LARAVEL. This website displays information about a company engaged in buying used office goods, as for its features such as homepage, about us, services, gallery, articles and contact us.This website has also been integrated with WhatsApp features.",
+      "Rizqi Computindo is a website that I created using HTML, PHP, CSS, JavaScript, and Laravel. This website displays information about a company engaged in buying used office goods, featuring pages like homepage, about us, services, gallery, articles, and contact us. This website has also been integrated with WhatsApp for customer inquiries.",
     livePreviewURL: "",
     repositoryURL: "",
     techStack: [
-      {
-        tech: "HTML",
-        logo: "/assets/svg/html.svg",
-      },
-      {
-        tech: "PHP",
-        logo: "/assets/svg/php.svg",
-      },
-      {
-        tech: "CSS",
-        logo: "/assets/svg/css.svg",
-      },
-      {
-        tech: "JavaScript",
-        logo: "/assets/svg/javascript.svg",
-      },
-      {
-        tech: "Laravel",
-        logo: "/assets/svg/laravel.svg",
-      },
+      { tech: "HTML", logo: "/assets/svg/html.svg" },
+      { tech: "PHP", logo: "/assets/svg/php.svg" },
+      { tech: "CSS", logo: "/assets/svg/css.svg" },
+      { tech: "JavaScript", logo: "/assets/svg/javascript.svg" },
+      { tech: "Laravel", logo: "/assets/svg/laravel.svg" },
     ],
   },
   {
     title: "Seribu Islands National Park Landing Pages",
     imageThumbnail: "/assets/images/Project4.webp",
     description:
-      "Seribu Islands National Park is a website that I created using HTML, PHP, CSS, JAVASCRIPT and LARAVEL. This website displays national and regional news information in Indonesia. The features are home, profile, excellent programs, services and permits, natural attractions, and news.",
+      "Seribu Islands National Park is a website that I created using HTML, PHP, CSS, JavaScript, and Laravel. This website displays national and regional news information in Indonesia, featuring sections like home, profile, excellent programs, services, permits, and news.",
     livePreviewURL: "",
     repositoryURL: "",
     techStack: [
-      {
-        tech: "HTML",
-        logo: "/assets/svg/html.svg",
-      },
-      {
-        tech: "PHP",
-        logo: "/assets/svg/php.svg",
-      },
-      {
-        tech: "CSS",
-        logo: "/assets/svg/css.svg",
-      },
-      {
-        tech: "JavaScript",
-        logo: "/assets/svg/javascript.svg",
-      },
-      {
-        tech: "Laravel",
-        logo: "/assets/svg/laravel.svg",
-      },
-    ],
-  },
-  {
-    title: "Service System for Cooperatives Landing Pages",
-    imageThumbnail: "/assets/images/Project5.webp",
-    description:
-      "Service System for Cooperatives is a website that I created using HTML, PHP, CSS, JAVASCRIPT and LARAVEL. This website displays information about cooperative savings and loan business rules in Kapuas Hulu.",
-    livePreviewURL: "",
-    repositoryURL: "",
-    techStack: [
-      {
-        tech: "HTML",
-        logo: "/assets/svg/html.svg",
-      },
-      {
-        tech: "PHP",
-        logo: "/assets/svg/php.svg",
-      },
-      {
-        tech: "CSS",
-        logo: "/assets/svg/css.svg",
-      },
-      {
-        tech: "JavaScript",
-        logo: "/assets/svg/javascript.svg",
-      },
-      {
-        tech: "Laravel",
-        logo: "/assets/svg/laravel.svg",
-      },
+      { tech: "HTML", logo: "/assets/svg/html.svg" },
+      { tech: "PHP", logo: "/assets/svg/php.svg" },
+      { tech: "CSS", logo: "/assets/svg/css.svg" },
+      { tech: "JavaScript", logo: "/assets/svg/javascript.svg" },
+      { tech: "Laravel", logo: "/assets/svg/laravel.svg" },
     ],
   },
   {
     title: "Haluan Kepri Landing Pages",
     imageThumbnail: "/assets/images/Project6.webp",
     description:
-      "Haluan Kepri is a website that I created using HTML, PHP, CSS, JAVASCRIPT and LARAVEL. This website displays national and regional news information in Indonesia.",
+      "Haluan Kepri is a website that I created using HTML, PHP, CSS, JavaScript, and Laravel. This website provides national and regional news information in Indonesia.",
     livePreviewURL: "",
     repositoryURL: "",
     techStack: [
-      {
-        tech: "HTML",
-        logo: "/assets/svg/html.svg",
-      },
-      {
-        tech: "PHP",
-        logo: "/assets/svg/php.svg",
-      },
-      {
-        tech: "CSS",
-        logo: "/assets/svg/css.svg",
-      },
-      {
-        tech: "JavaScript",
-        logo: "/assets/svg/javascript.svg",
-      },
-      {
-        tech: "Laravel",
-        logo: "/assets/svg/laravel.svg",
-      },
+      { tech: "HTML", logo: "/assets/svg/html.svg" },
+      { tech: "PHP", logo: "/assets/svg/php.svg" },
+      { tech: "CSS", logo: "/assets/svg/css.svg" },
+      { tech: "JavaScript", logo: "/assets/svg/javascript.svg" },
+      { tech: "Laravel", logo: "/assets/svg/laravel.svg" },
     ],
   },
   {
     title: "Gadget Shop Landing Page",
     imageThumbnail: "/assets/images/Project7.webp",
     description:
-      "Gadget Shop is a website that I created using HTML, PHP, CSS, JavaScript, and Laravel. This website displays information about companies that sell cell phones and laptops. This website also features recommendations for users, specifications, and prices of cell phones and laptops, as well as reviews, ratings and feedback from customers. This website aims to attract and inform potential buyers about the quality and innovation of cellphones and laptops that they want to buy.",
+      "Gadget Shop is a website that I created using HTML, PHP, CSS, JavaScript, and Laravel. This website provides information about companies that sell cellphones and laptops, with features like recommendations, specifications, prices, reviews, ratings, and customer feedback.",
     livePreviewURL: "",
     repositoryURL: "",
     techStack: [
-      {
-        tech: "HTML",
-        logo: "/assets/svg/html.svg",
-      },
-      {
-        tech: "PHP",
-        logo: "/assets/svg/php.svg",
-      },
-      {
-        tech: "CSS",
-        logo: "/assets/svg/css.svg",
-      },
-      {
-        tech: "JavaScript",
-        logo: "/assets/svg/javascript.svg",
-      },
-      {
-        tech: "Laravel",
-        logo: "/assets/svg/laravel.svg",
-      },
-      {
-          tech: "MySQL",
-          logo: "/assets/svg/mysql.svg",
-        },
+      { tech: "HTML", logo: "/assets/svg/html.svg" },
+      { tech: "PHP", logo: "/assets/svg/php.svg" },
+      { tech: "CSS", logo: "/assets/svg/css.svg" },
+      { tech: "JavaScript", logo: "/assets/svg/javascript.svg" },
+      { tech: "Laravel", logo: "/assets/svg/laravel.svg" },
+      { tech: "MySQL", logo: "/assets/svg/mysql.svg" },
     ],
   },
-    {
-      title: "My Firts Personal Portfolio",
-      imageThumbnail: "/assets/images/Project0.webp",
-      description:
-        "This is firts a personal branding project that I created using HTML, CSS, JavaScript, and Boostrap. The project consists of a website that showcases my skills, education, and personality. The website has six sections: Home, About Me, Services, Portfolio, Certificates, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers and collaborators.",
-      livePreviewURL: "https://yogaswidhana.github.io/Portofolio/",
-      repositoryURL: "https://github.com/yogaswidhana/Portofolio",
-      techStack: [
-        {
-          tech: "HTML",
-          logo: "/assets/svg/html.svg",
-        },
-        {
-          tech: "CSS",
-          logo: "/assets/svg/css.svg",
-        },
-        {
-          tech: "JavaScript",
-          logo: "/assets/svg/javascript.svg",
-        },
-        {
-          tech: "Boostrap",
-          logo: "/assets/svg/bootstrap.svg",
-        },
-      ],
-    }
-   {
-      title: "Sistem POS Kasir Berbasis Web",
-      imageThumbnail: "/assets/images/Project9.webp",
-      description:
-        "This is firts a personal branding project that I created using Technology HTML, PHP, CSS, JavaScript, MySQL and Boostrap.",
-      livePreviewURL: "https://poskasirarte.000webhostapp.com/login.php",
-      repositoryURL: "https://github.com/yogaswidhana/poskasirarte",
-      techStack: [
-        {
-          tech: "HTML",
-          logo: "/assets/svg/html.svg",
-        },
-          {
-        tech: "PHP",
-        logo: "/assets/svg/php.svg",
-        },
-         {
-          tech: "CSS",
-          logo: "/assets/svg/css.svg",
-        },
-        {
-          tech: "JavaScript",
-          logo: "/assets/svg/javascript.svg",
-        },
-        {
-          tech: "Boostrap",
-          logo: "/assets/svg/bootstrap.svg",
-        },
-          {
-          tech: "MySQL",
-          logo: "/assets/svg/mysql.svg",
-        },
-      ],
-    }
-  ];
+  {
+    title: "Sistem POS Kasir Berbasis Web",
+    imageThumbnail: "/assets/images/Project9.webp",
+    description:
+      "This is a web-based POS cashier system project that I created using HTML, PHP, CSS, JavaScript, MySQL, and Bootstrap.",
+    livePreviewURL: "https://poskasirarte.000webhostapp.com/login.php",
+    repositoryURL: "https://github.com/yogaswidhana/poskasirarte",
+    techStack: [
+      { tech: "HTML", logo: "/assets/svg/html.svg" },
+      { tech: "PHP", logo: "/assets/svg/php.svg" },
+      { tech: "CSS", logo: "/assets/svg/css.svg" },
+      { tech: "JavaScript", logo: "/assets/svg/javascript.svg" },
+      { tech: "Bootstrap", logo: "/assets/svg/bootstrap.svg" },
+      { tech: "MySQL", logo: "/assets/svg/mysql.svg" },
+    ],
+  },
+];
